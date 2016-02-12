1100b34289eccda4051560caf999146c5f51f080
Joseph <josephbates25@gmail.com>
2016-02-12 14:19:46 -0600

    Added Ring Alarm and Script Execute Methods, cleaned up args parsing

--- Main.py
+++ Main.py
@@ -6,9 +6,26 @@ import random
 import argparse
 import sys
 
+ALARM_SOUND_ENVIRONMENT_VARIABLE = "PyTimerAlarm"
+
+def init_parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("minutes", action="store", nargs="*")
+    parser.add_argument("-seconds", "-s", action="store_true", help="Set if input should be interpreted as seconds")
+    parser.add_argument("-repeat", "-r",  nargs="*", type=int, help="Specifies fixed (or random if 2 values given) number of repetitions")
+    parser.add_argument("-verbose", "-v", action="store_true", help="Call to turn off printing")
+    parser.add_argument("-sound", action="store", nargs="?", help="Set new sound to be played. Set to 'None' for no sound.")
+    parser.add_argument("-exec", "-e", action="store", nargs="*", help="Set new sound to be played")
+    parser.add_argument("-stop", action="store_true", help="Starts stopwatch")
+    parser.add_argument("-stand", action="store", nargs=1, help="Starts stopwatch")
 
 
-def seconds_to_time(s):
+    return parser.parse_args()
+
+
+#Converts seconds value to formatted hour:date:second string
+#Ex. 4432 seconds -> "01:13:52"
+def seconds_to_time_string(s):
     minutes = 0
     seconds = 0
     hours = 0
@@ -22,7 +39,6 @@ def seconds_to_time(s):
 
     return "%02d" % (hours,) + ":" + "%02d" % (minutes,) + ":" + "%02d" % (seconds,)
 
-a = seconds_to_time(9485)
 
 
 
@@ -43,19 +59,25 @@ def standby(seconds):
     for x in range(int(seconds)):
         if verbose:
             count = int(seconds) - index
-            sys.stdout.write("\rStandby:..." + seconds_to_time(count))
+            sys.stdout.write("\rStandby:..." + seconds_to_time_string(count))
             sys.stdout.flush()
             index += 1
         time.sleep(1)
 
+#Check OS Env for sound path
+def getSoundFilePath():
+    import os
+    return os.environp[ALARM_SOUND_ENVIRONMENT_VARIABLE]
+
+
 def ring_timer_sec(seconds):
-    print_verbose("Ringing in " + seconds_to_time(seconds))
+    print_verbose("Ringing in " + seconds_to_time_string(seconds))
     print_verbose("Beginning timer: ")
     index = 0
     for x in range(int(seconds)):
         if verbose:
             count = int(seconds) - index
-            sys.stdout.write("\rCountdown:..." + seconds_to_time(count))
+            sys.stdout.write("\rCountdown:..." + seconds_to_time_string(count))
             sys.stdout.flush()
             index += 1
         time.sleep(1)
@@ -66,15 +88,51 @@ def ring_timer_sec(seconds):
     if parsed_args.exec is not None:
         os.system("./" + str(parsed_args.exec[0]))
 
+def timer_sec(seconds):
+    print_verbose("Complete in " + seconds_to_time_string(seconds))
+    print_verbose("Beginning timer: ")
+    index = 0
+    for x in range(int(seconds)):
+        if verbose:
+            count = int(seconds) - index
+            sys.stdout.write("\rCountdown:..." + seconds_to_time_string(count))
+            sys.stdout.flush()
+            index += 1
+        time.sleep(1)
+    print_verbose("\nTimer complete.")
+    if parsed_args.exec:
+        execute_scripts()
+    else:
+        ring_alarm()
+
+
+
+#TODO: Add checking for sound file override in parsed args
+def ring_alarm():
+    path = getSoundFilePath()
+    if path != "":
+        if os._exists(path):
+            os.system("afplay " + path)
+        else:
+            print_verbose("The file " + path + " cannot be found. Please verify it exists.")
+
+def execute_scripts():
+    if parsed_args.exec is not None:
+       for script in parsed_args.exec:
+           if os.exists(script):
+               os.system("./" + str(script))
+           else:
+                print_verbose("The shell script " + script + " could not be found. Please verify it exists.")
+
 
 def constructSummary(interval_length, repeat):
     summary = "Timer will ring "
     if type(interval_length) == tuple:
         summary += "between " + \
-                  seconds_to_time(interval_length[0]) + " and " + \
-                  seconds_to_time(interval_length[1])
+                   seconds_to_time_string(interval_length[0]) + " and " + \
+                   seconds_to_time_string(interval_length[1])
     else:
-        summary += seconds_to_time(interval_length)
+        summary += seconds_to_time_string(interval_length)
 
     summary += " with "
     if type(repeat) == tuple:
@@ -107,25 +165,15 @@ def list_to_string(l):
 def stopwatch():
     time_counted = 0
     while True:
-        sys.stdout.write("\rTime:..." + seconds_to_time(time_counted))
+        sys.stdout.write("\rTime:..." + seconds_to_time_string(time_counted))
         sys.stdout.flush()
         time.sleep(1)
         time_counted += 1
 
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("minutes", action="store", nargs="*")
-parser.add_argument("-seconds", "-s", action="store_true", help="Set if input should be interpreted as seconds")
-parser.add_argument("-repeat", "-r",  nargs="*", type=int, help="Specifies fixed (or random if 2 values given) number of repetitions")
-parser.add_argument("-verbose", "-v", action="store_true", help="Call to turn off printing")
-parser.add_argument("-sound", action="store", nargs="?", help="Set new sound to be played. Set to 'None' for no sound.")
-parser.add_argument("-exec", "-e", action="store", nargs="*", help="Set new sound to be played")
-parser.add_argument("-stop", action="store_true", help="Starts stopwatch")
-parser.add_argument("-stand", action="store", nargs=1, help="Starts stopwatch")
 
-
-parsed_args = parser.parse_args()
+parsed_args =  init_parse_args()
 
 if len(sys.argv) == 1 or parsed_args.stop:
     stopwatch()
@@ -162,4 +210,7 @@ else:
         repeat = 1
 
     ring_timer_interval(minutes, repeat)
+
+def __main__():
+    print("Hello World!")
 
